7893b4abcad4ab495c8d6683a73518d83efd5d2a
Leoshe22 <leosparacino4@gmail.com>
2022-02-20 18:50:46 -0500

    change IsEnemyTo to use CompareTag instead of ==

--- Assets/Scripts/Core/Tags.cs
+++ Assets/Scripts/Core/Tags.cs
@@ -9,8 +9,8 @@ public static class Tags
     public const string Player = "Player";
 
     public static bool IsEnemyTo(this MonoBehaviour mono, string otherTag) {
-        if (otherTag == Enemy) return mono.tag == Player;
-        if (otherTag == Player) return mono.tag == Enemy;
+        if (otherTag == Enemy) return mono.CompareTag(Player);
+        if (otherTag == Player) return mono.CompareTag(Enemy);
         return false;
     }
 
